8d6078db58f0602d40be5d7760357fd0c28b1d4c
cfardev <cfar.dev@gmail.com>
2026-05-18 21:15:54 -0600

    feat(inventory-stock): add AuthModule to inventory stock module imports

--- apps/api/src/modules/inventory-stock/inventory-stock.module.ts
+++ apps/api/src/modules/inventory-stock/inventory-stock.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../../auth/auth.module.js";
 import { InventoryStockController } from "./inventory-stock.controller.js";
 import { InventoryStockService } from "./inventory-stock.service.js";
 
 @Module({
+	imports: [AuthModule],
 	controllers: [InventoryStockController],
 	providers: [InventoryStockService],
 	exports: [InventoryStockService],
